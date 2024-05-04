63f9505b4f59f14d83c1da2e5c65d0908d5af007
Ravi Bhaskar <ravi.bhaskar@tecvololabs.com>
2024-05-04 22:16:15 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
+import Login from "./Components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./Components/ProtectedRoute";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello Mr Ravi
+        <nav>
+          <a href="/">Home</a>
+          <a href="/contact">Contact</a>
+          <a href="/about">About</a>
+          <a href="/login">Login</a>
+        </nav>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={< ProtectedRoute/>}>
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
